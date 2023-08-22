5ce7a777bcbb9873594f090561e2fdecc8f82903
gogongkong <123641568+gogongkong@users.noreply.github.com>
2023-08-22 22:08:18 +0900

    잠시 쉬어가기
    내가 뭘 하고싶은지 생각해보자
    무작정 자기위로처럼 공부하지 말고 어떤일을 하고싶은지 그리고 그에 맞는 준비는 어떻게 해야하는지 생각하자.

---
+++ C_Newbee_Study/Class101/HeaderAndSource/HeaderAndSource/Source.cpp
@@ -0,0 +1,38 @@
+#include<cstdio>
+// #include ...
+// 작성된 헤더파일에 존재하는 코드를 모두 복사 붙여넣기 하는 구문
+// 그 외에 특별한 것은 없으나 이로 인해 C++는 시스템 함수를 사용할 수 있게 된다.
+
+
+// cstdio
+// 표준 입출력 헤더
+// 콘솔 입출력과 파일 입출력에 관한 함수들이 포함된다.
+// ex : printf , scanf, fseek, fopen...
+
+#include<cmath>
+// cmath
+// 수학 연산 관련 함수를 포함한다.
+// sin, cos, pow, log ...
+
+#include<cstdlib>
+// 각종 활용할 수 있는 라이브러리 함수들을 포함
+// malloc : 동적 메모리를 할당한다.
+// free : 동적 메모리를 해제한다.
+// rand : 난수를 생성한다
+
+#include<ctime> // 시간 관련 함수를 사용할 수 있게 된다.
+
+// 좌 C++ , 우 C
+// cstdio = stdio.h
+// cmath = math.h
+// cstdlib = stdlib.h
+// ctime = time.h
+ 
+
+int main() {
+	printf("Hello World\n");
+
+	
+
+	return 0;
+}
